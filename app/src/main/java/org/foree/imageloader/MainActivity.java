a17165fbce027c2d00818b7931fede0cccca86d1
foree <beijing.2008.lm@163.com>
2016-09-30 15:47:42 +0800

    增加RecyclerView显示模式的切换

--- app/src/main/java/org/foree/imageloader/MainActivity.java
+++ app/src/main/java/org/foree/imageloader/MainActivity.java
@@ -1,12 +1,13 @@
 package org.foree.imageloader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
 
-        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(imageAdapter = new ImageAdapter());
     }
 
@@ -32,7 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            return new ImageHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.image_holder, parent, false));
+            if( recyclerView.getLayoutManager() instanceof LinearLayoutManager)
+                return new ImageHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.image_holder_linearlayout, parent, false));
+            else
+                return new ImageHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.image_holder_staggerd, parent, false));
         }
 
         @Override
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return 30;
         }
+
     }
 
     class ImageHolder extends RecyclerView.ViewHolder{
@@ -54,4 +59,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.action_change_card:
+                recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+                recyclerView.setAdapter(imageAdapter = new ImageAdapter());
+                break;
+            case R.id.action_change_list:
+                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+                recyclerView.setAdapter(imageAdapter = new ImageAdapter());
+                break;
+        }
+        return true;
+    }
 }
