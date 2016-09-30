e4e7287a63fa9b84e5d07f336d1a3b29f56b7faf
foree <beijing.2008.lm@163.com>
2016-09-30 11:55:56 +0800

    增加RecyclerView

--- app/src/main/java/org/foree/imageloader/MainActivity.java
+++ app/src/main/java/org/foree/imageloader/MainActivity.java
@@ -2,12 +2,55 @@ package org.foree.imageloader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 切换浏览模式
+    RecyclerView recyclerView;
+    ImageAdapter imageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(imageAdapter = new ImageAdapter());
     }
+
+    class ImageAdapter extends RecyclerView.Adapter<ImageHolder> {
+
+        @Override
+        public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ImageHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.image_holder, parent));
+        }
+
+        @Override
+        public void onBindViewHolder(ImageHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    class ImageHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+
+        public ImageHolder(View itemView) {
+            super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.image_view);
+        }
+    }
+
 }
