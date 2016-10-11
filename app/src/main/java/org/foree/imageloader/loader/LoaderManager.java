d161da619bf93ec1010644e593f92631e32e9c7e
foree <beijing.2008.lm@163.com>
2016-10-11 18:17:01 +0800

    使loaderManager的注册生效

--- app/src/main/java/org/foree/imageloader/loader/LoaderManager.java
+++ app/src/main/java/org/foree/imageloader/loader/LoaderManager.java
@@ -1,5 +1,8 @@
 package org.foree.imageloader.loader;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by foree on 16-9-30.
  * 管理加载器的注册与注销
@@ -9,7 +12,18 @@ public class LoaderManager {
 
     private static LoaderManager mInstance;
 
-    private LoaderManager(){}
+    private Map<String, AbsLoader> mLoaderList;
+
+    private final String SCHEMA_NET = "http";
+    private final String SCHEMA_NET2 = "https";
+    private final String SCHEMA_LOCAL = "file";
+
+    private LoaderManager(){
+        mLoaderList = new HashMap<>();
+        registerLoader(SCHEMA_NET, new NetLoader());
+        registerLoader(SCHEMA_NET2, new NetLoader());
+        registerLoader(SCHEMA_LOCAL, new LocalLoader());
+    }
 
     // singleton
     public static LoaderManager getInstance(){
@@ -26,31 +40,30 @@ public class LoaderManager {
     /**
      * 注册图片加载器
      */
-    void registerLoader(){
-
+    public void registerLoader(String schema, AbsLoader loader){
+        mLoaderList.put(schema, loader);
     }
 
     /**
      * 取消注册图片加载器
      */
-    void unRegisterLoader(){}
+    public void unRegisterLoader(String schema){
+        mLoaderList.remove(schema);
+    }
 
     /**
      * 根据Uri选择合适的图片加载器
      */
     public AbsLoader getLoader(String Uri){
-        AbsLoader loader;
 
-        //以file://开头
-        if( Uri.startsWith("file://")){
-            loader = new LocalLoader();
-        }else if( Uri.startsWith("http://") || Uri.startsWith("https://")){
-            loader = new NetLoader();
-        }else {
-            loader = new NullLoader();
-        }
+        // 提取schema
+        String schema = Uri.split("://")[0];
 
-        return loader;
+        if( mLoaderList.containsKey(schema))
+            return mLoaderList.get(schema);
+        else
+            return new NullLoader();
+
     }
 
 }
